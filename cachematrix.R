709f77e5ec2c833b11e40b177cae36b0eaf06aeb
mcc5635 <85896593+mcc5635@users.noreply.github.com>
2021-07-25 11:02:40 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+setwd('https://github.com/mcc5635/ProgrammingAssignment2-1.git')
+##
+## makeCacheMatrix purpose: invertible square matrix
+## Returns 'x' inverse matrix
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  r <- NULL
+  set <- function(y) {
+    x <<- y
+    r <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) r <<- solve
+  getsolve <- function() r
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+##
+## "Solve" was substituted for "mean"
+## Purpose: catche matrix inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  r <- x$getsolve()
+  if(!is.null(r)) {
+    message("getting inversed matrix")
+    return(r)
+  }
+  data <- x$get()
+  r <- solve(data, ...)
+  x$setsolve(r)
+  r
 }
+
+
+
+
